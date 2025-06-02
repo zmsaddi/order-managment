125e61b8cf8e0a8be1e6f7e10d4c75fe6b62457a
zmsaddi <35869249+zmsaddi@users.noreply.github.com>
2025-06-03 00:06:40 +0200

    auth.service.js

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'1
 
 // إنشاء عميل Supabase
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
