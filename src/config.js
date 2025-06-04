58507dc01b7b4a1b8b738e64004ac0aa804542d4
zmsaddi <35869249+zmsaddi@users.noreply.github.com>
2025-06-04 03:54:05 +0200

    Delete src directory

--- src/config.js
+++
@@ -1,5 +0,0 @@
-// تكوين متغيرات البيئة لـ Supabase
-export const config = {
-  supabaseUrl: 'https://mrocuyajltnvhiiigwqb.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yb2N1eWFqbHRudmhpaWlnd3FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg5ODk4NTQsImV4cCI6MjA2NDU2NTg1NH0.zzFEJKO6rHm5GypGR4QSFBFMwCBibdgOlkzL5V08Rf4'
-}
